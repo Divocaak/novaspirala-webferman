df7c506a7845dad0af4249f78d585d93b33b76e7
Vojtěch divoký <divokyvojta@gmail.com>
2026-08-03 20:02:47 +0200

    začít psát changelog  #83

---
+++ src/routes/(designed)/changelog/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import MarkdownIt from 'markdown-it';
+	const md = new MarkdownIt();
+	const source = `
+### v0.2.0, release dd. mm. yyyy
+#### přidáno
+#### upraveno
+#### opraveno
+    `;
+
+	$: html = md.render(source);
+</script>
+
+<h2>Changelog</h2>
+<div class="markdown">{@html html}</div>
